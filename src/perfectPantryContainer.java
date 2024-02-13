bc12c28c99273712e9d38b51c1c2fb990ce78c1f
SarahAsghar <sarahasghar126@gmail.com>
2024-02-12 23:00:18 -0500

    Added fields and getter methods in perfectPantryContainer class

--- src/perfectPantryContainer.java
+++ src/perfectPantryContainer.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
 public class perfectPantryContainer {
-
+	private String name;
+	private ArrayList<Item> listOfItems;
+	
+	public perfectPantryContainer(String n) {
+		this.name = n;
+		listOfItems = new ArrayList<Item>();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	
 }
