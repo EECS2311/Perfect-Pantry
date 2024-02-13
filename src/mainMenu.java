a8d403ab7ae1e481ed1349d6e05af90cbaa97d57
SarahAsghar <sarahasghar126@gmail.com>
2024-02-12 22:12:23 -0500

    Setup mainMenu panel (holds all of the components of the mainMenuGUI)

--- src/mainMenu.java
+++ src/mainMenu.java
@@ -2,10 +2,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class mainMenu {
 	
 	static JFrame frame;
+	private JPanel mainMenuPanel;
+	
+	
 	public static void main(String[] args) {
 		mainMenu m = new mainMenu(); //calls constructer
 	}
@@ -17,6 +21,21 @@ public class mainMenu {
 		frame.setVisible(true);
 		frame.setMinimumSize(new Dimension(800, 800));
 		frame.getContentPane().setBackground(new Color(245, 223, 162));
+		
+		initializeMainMenuGUI();
+		
+	}
+	
+	public void initializeMainMenuGUI() {
+		//Initialse mainMenuPanel
+		mainMenuPanel = new JPanel(); //will hold all the components of mainMenuGUI
+		mainMenuPanel.setLayout(null);
+		frame.getContentPane().add(mainMenuPanel);
+		
+		
+		
+		
+		
 		
 	}
 
