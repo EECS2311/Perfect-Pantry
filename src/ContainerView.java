69bf7bb28a66530e50a48ae292b10ccfe3f1be49
SarahAsghar <sarahasghar126@gmail.com>
2024-02-14 00:35:30 -0500

    added sample gui to test containerview

--- src/ContainerView.java
+++ src/ContainerView.java
@@ -1,4 +1,41 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ContainerView {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ContainerView implements ActionListener {
+	private JPanel containerView;
+	private JButton back;
+	private boolean hasclicked;
+	
+	ContainerView(){
+		hasclicked = false;
+	}
+	
+	public void setupContainerViewGUI(boolean b) {
+		if(hasclicked == false) {
+			containerView = new JPanel();
+			back = new JButton("Back");
+			back.addActionListener(this);
+			containerView.add(back);
+			Home.getFrame().add(containerView);
+			hasclicked = true;
+		}
+		
+		containerView.setVisible(b);
+		Home.getFrame().revalidate();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == back) {
+			setupContainerViewGUI(false);
+			Home.setupHomeGUI(true);
+			
+		}
+		
+	}
 
 }
