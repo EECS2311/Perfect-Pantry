772d13dc23cca9ffd3c19da60b3cb7bba41b0105
nina2dev <nina2dev@gmail.com>
2024-02-14 16:51:31 -0500

    Add JavaDoc comments

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,18 +1,9 @@
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-
 public class FirstClass {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
 
-		Set<GenericTag<FoodGroup>> tags = new HashSet<>();
-		tags.add(new GenericTag<>(FoodGroup.DAIRY));
-		Date day = new Date();
-		Item i = Item.getInstance("Milk", tags, new GenericTag<>(FoodFreshness.FRESH), 5, day);
-		System.out.println(i);
 
 	}
 
