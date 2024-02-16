dec5e414cde084b9ebea95c260c1798613110e42
wnm <wnm@live.ca>
2024-02-16 03:51:07 -0500

    Updated User Class and Container Class.

--- src/domain/logic/Container.java
+++ src/domain/logic/Container.java
@@ -4,6 +4,7 @@ import gui.ContainerView;
 import gui.Home;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Container {
 	private String name;
@@ -15,9 +16,31 @@ public class Container {
 		listOfItems = new ArrayList<>();
 		containerViewgui = new ContainerView(home); // Pass Home instance to ContainerView
 	}
+	
+	public Container(String name) {
+		this.name = name;
+		listOfItems = new ArrayList<>();
+	}
+	
+	public Container(Container container) {
+		this.name = container.getName();
+		this.listOfItems = new ArrayList<>();
+		ArrayList<Item> containerItems = container.getItems();
+		for(Item item : containerItems) {
+			this.listOfItems.add(item);
+		}		
+	}
+	
+	public Container(String name, ArrayList<Item> items) {
+		this.name = name;
+		this.listOfItems = new ArrayList<>();
+		for(Item item : items) {
+			this.listOfItems.add(item);
+		}		
+	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 
 	/**
@@ -25,6 +48,21 @@ public class Container {
 	 */
 	public void addNewItem(Item item) {
 		listOfItems.add(item);
+	}
+	
+	public void removeItem(Item item) {
+		listOfItems.remove(item);
+	}
+	
+	public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Container container = (Container) o;
+        return Objects.equals(this.name, container.getName());
+    }
+	
+	public ArrayList<Item> getItems(){
+		return this.listOfItems;
 	}
 
 	public void getGUI() {
--- src/domain/logic/User.java
+++ src/domain/logic/User.java
@@ -5,15 +5,15 @@ import java.util.*;
  */
 public class User {
 	private String name;
+	private ArrayList<Container> listOfContainers;
 	private ArrayList<Item> listOfItems;
-	
 	// Constructors are private to enforce the use of factory methods for instance creation.
 	/*
 	 * Constructor that adds a default new user.
 	 */
 	private User() {
 		this.setName("User");
-		this.listOfItems = new ArrayList<>();
+		this.listOfContainers = new ArrayList<>();
 	}
 	/*
 	 * Constructor that adds a new user where the name is given.
@@ -21,19 +21,20 @@ public class User {
 	 */
 	private User(String name) {
 		this.setName(name);
-		this.listOfItems = new ArrayList<>();
+		this.listOfContainers = new ArrayList<>();
 	}
 	/*
 	 * Constructor that adds a new user where the name and the list of items owned is given.
 	 * @param name The name of the user.
 	 * @param listOfItems The list of items owned by the user.
 	 */
-	private User(String name, ArrayList<Item> listOfItems) {
+	private User(String name, ArrayList<Container> listOfContainers) {
 		this.setName(name);
-		this.listOfItems = new ArrayList<>();
-		for(Item item : listOfItems) {
-			Item copiedItem = Item.getInstance(item);
-			this.listOfItems.add(copiedItem);
+		this.listOfContainers = new ArrayList<>();
+		for(Container container : listOfContainers) {
+			this.listOfContainers.add(container);
+			this.listOfItems.addAll(container.getItems());
+			
 		}
 	}
 	/*
@@ -42,38 +43,42 @@ public class User {
 	 */
 	private User(User user) {
 		this.setName(user.getName());
-		this.listOfItems = new ArrayList<>();
-		ArrayList<Item> userItems = user.getListOfItems();
-		for(Item item : userItems) {
-			Item copiedItem = Item.getInstance(item);
-			this.listOfItems.add(copiedItem);
+		this.listOfContainers = new ArrayList<>();
+		ArrayList<Container> userContainers = user.getListOfContainers();
+		for(Container container : userContainers) {
+			this.listOfContainers.add(container);
+			this.listOfItems.addAll(container.getItems());
 		}
 	}
-	
-	//Factory method to create an instance of User with default properties.
+	/*
+	 * Factory method to create an instance of User with default properties.
+	 */
 	public static User getInstance() {
-		User user = User.getInstance();
+		User user = new User();
 		return user;
 	}
 	/*
 	 * Factory method to create an instance of User with assigned name.
 	 * @param name The name of the user.
+	 * @return New user object.
 	 */
 	public static User getInstance(String name) {
-		User user = User.getInstance(name);
+		User user = new User(name);
 		return user;
 	}
 	/*
-	 * Factory method to create an instance of User with assigned name and list of items owned.
+	 * Factory method to create an instance of User with assigned name and list of containers.
 	 * @param name The name of the user.
-	 * @param listOfItems The list of items owned by the user.
+	 * @param listOfItems The list of containers owned by the user.
+	 * @return New user object.
 	 */
-	public static User getInstance(String name, ArrayList<Item> listOfItems) {
-		User user = User.getInstance(name, listOfItems);
+	public static User getInstance(String name, ArrayList<Container> listOfContainers) {
+		User user = new User(name, listOfContainers);
 		return user;
 	}
 	/*
-	 * Returns the name of the user.
+	 * Gets the name of the user.
+	 * @return The name of the user.
 	 */
 	public String getName() {
 		return this.name;
@@ -86,12 +91,80 @@ public class User {
 		this.name = name;
 	}
 	/*
-	 * Returns the list of items owned by the user.
+	 * Adds a new container to the user.
+	 * @param container The container to be added.
+	 */
+	public void addContainer(Container container) {
+		this.listOfContainers.add(container);
+	}
+	/*
+	 * Gives a list of all the containers owned by the user.
+	 * @return the list of containers owned by the user. 
+	 */
+	public ArrayList<Container> getListOfContainers() {
+		ArrayList<Container> list = new ArrayList<>();
+		for(Container container : this.listOfContainers) {
+			Container con = new Container(container);
+			list.add(con);
+		}
+		return list;
+	}
+	/*
+	 * Gives a list of items owned by the user.
+	 * @return The list of items owned by the user.
 	 */
 	public ArrayList<Item> getListOfItems(){
-		return this.listOfItems;
+		ArrayList<Item> list = new ArrayList<>();
+		for(Item item : this.listOfItems) {
+			list.add(item);
+		}
+		return list;
 	}
-	
-	
+	/*
+	 * Finds the container by name in the user's list of containers.
+	 * @param name Name of the container.
+	 * @return Returns the container if user owns the container, returns null if user does not.
+	 * @throws IllegalArgumentException If the name is empty.
+	 */
+	public Container findContainer(String name) {
+		if(name.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		for(Container container: this.listOfContainers) {
+			if(container.getName().equals(name)) {
+				return container;
+			}
+		}
+		return null;
+	}
+	/*
+	 * Adds an item to a container owned by the user.
+	 * @param containerName The name of the container that the item will be stored in.
+	 * @param item The item to be added.
+	 * @throws IllegalArgumentException If the user does not own the container.
+	 */
+	public void addItemToUser(String containerName, Item item) {
+		Container con = findContainer(containerName);
+		if(con != null) {
+			con.addNewItem(item);
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
+	}
+	/*
+	 * Removes an item from a container owned by the user.
+	 * @param containerName The name of the container that the item will be stored in.
+	 * @param item The item to be removed.
+	 * @throws IllegalArgumentException If the user does not own the container.
+	 */
+	public void removeItemFromUser(String containerName, Item item) {
+		Container con = findContainer(containerName);
+		if(con != null) {
+			con.removeItem(item);
+		}
+		else {
+			throw new IllegalArgumentException();
+		}
+	}
 }
-
