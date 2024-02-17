3bfbf11cc48340b60d0c1782b605ea00d638e777
nina2dev <nina2dev@gmail.com>
2024-02-16 23:20:46 -0500

    Adding JavaDoc to DB, Container, Tag

--- src/database/DB.java
+++ src/database/DB.java
@@ -6,21 +6,44 @@ import java.util.Map;
 import domain.logic.Container;
 import domain.logic.Item;
 
+/**
+ * The {@code DB} class represents a simple database for storing and managing containers and their associated items.
+ * This class provides methods to add and retrieve containers and items, remove items, and print all items within containers.
+ */
 public class DB {
 
 	private HashMap<String, Container> containers = new HashMap<String, Container>();
 
 	private HashMap<Container, HashMap<String, Item>> items = new HashMap<Container, HashMap<String, Item>>();
 
+	/**
+	 * Retrieves a {@link Container} by its name.
+	 *
+	 * @param containerName The name of the container to retrieve.
+	 * @return The {@link Container} object if found, {@code null} otherwise.
+	 */
 	public Container getContainer(String containerName) {
 		return containers.get(containerName);
 	}
 
+	/**
+	 * Adds a new container to the database.
+	 *
+	 * @param containerName The name of the container to add.
+	 * @param c The {@link Container} object to be added.
+	 */
 	public void addContainer(String containerName, Container c) {
 		containers.put(containerName, c);
 
 	}
 
+	/**
+	 * Adds an item to a specific container.
+	 *
+	 * @param c The container to which the item will be added.
+	 * @param name The name of the item.
+	 * @param ite The {@link Item} object to be added.
+	 */
 	public void addItem(Container c, String name, Item ite) {
 
 		// Check if container has associated items.
@@ -40,6 +63,12 @@ public class DB {
 
 	}
 
+	/**
+	 * Removes an item from a specified container.
+	 *
+	 * @param c The container from which the item will be removed.
+	 * @param name The name of the item to be removed.
+	 */
 	public void removeItem(Container c, String name, Item ite) {
 
 		// Any method that calls removeItem() will ensure that the item exists.
@@ -47,7 +76,13 @@ public class DB {
 
 	}
 
-	// Method to retrieve an Item by its Container and name.
+	/**
+	 * Retrieves an {@link Item} by its container and name.
+	 *
+	 * @param container The container in which the item is stored.
+	 * @param itemName The name of the item to retrieve.
+	 * @return The {@link Item} object if found, {@code null} otherwise.
+	 */
 	public Item getItem(Container container, String itemName) {
 		if (items.containsKey(container) && items.get(container).containsKey(itemName)) {
 			return items.get(container).get(itemName);
@@ -56,6 +91,9 @@ public class DB {
 		return null;
 	}
 
+	/**
+	 * Prints all items within each container to the standard output.
+	 */
 	public void printItems() {
 		for (Map.Entry<Container, HashMap<String, Item>> containerEntry : items.entrySet()) {
 			Container container = containerEntry.getKey();
