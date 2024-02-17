c66266439b29a4a69a7ca5e71785c47078558d1a
Mich <paperprimer00@gmail.com>
2024-02-16 21:50:35 -0500

    Quick Test for removing items

--- src/test/DeleteItemViewTest.java
+++ src/test/DeleteItemViewTest.java
@@ -1,9 +1,14 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import database.DB;
+import domain.logic.Container;
+import domain.logic.Item;
 
 class DeleteItemViewTest {
 
@@ -12,8 +17,18 @@ class DeleteItemViewTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testRemove() {
+		DB data = new DB();
+		Container c = new Container("Cheese");
+		Item item = Item.getInstance("Feta", 25);
+		data.addContainer("Cheese", c);
+		data.addItem(c, "Feta", item);
+
+		assertNotNull(data.getItem(c, "Feta"));
+		data.removeItem(c, "Feta", item);
+
+		assertTrue(data.getItem(c, "Feta") == null);
+
 	}
 
 }
