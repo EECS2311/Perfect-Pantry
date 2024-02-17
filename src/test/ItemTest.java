92eab7c09d81ba153a11c4e30086b4f4489071f3
nina2dev <nina2dev@gmail.com>
2024-02-16 20:55:28 -0500

    Start ItemTest

--- src/test/ItemTest.java
+++ src/test/ItemTest.java
@@ -3,8 +3,19 @@ package test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import domain.logic.Item;
+import org.junit.jupiter.api.BeforeEach;
 
 
 class ItemTest {
+    Item item1;
+    Item item2;
+    Item item3;
+
+    @BeforeEach
+    void init(){
+
+    }
+
+
 
 }
