e412126ae82a512f6baaae81e3f6e2d6e81f69f1
Mich <paperprimer00@gmail.com>
2024-02-21 10:00:49 -0500

    Updated Basic database schema

--- src/database/initSchema.sql
+++ src/database/initSchema.sql
@@ -1,7 +1,15 @@
-CREATE Table Item(
+CREATE TYPE Food_Group AS ENUM ('Grain', 'Protein', 'Vegetable', 'Fruit', 'Dairy');
+CREATE TYPE Freshness AS ENUM ('Expired', 'Fresh', 'Near_Expiry');
+
+CREATE TABLE item(
     name varchar(50) PRIMARY KEY,
+    container varchar(50) REFERENCES container(container_name),
     quantity INTEGER, 
-    expiry date
+    expiry Date,
+    fg Food_Group,
+    fresh Freshness
 );
 
-
+CREATE TABLE container(
+    container_name varchar(50) PRIMARY KEY
+);
