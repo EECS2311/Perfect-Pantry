26417d994a3969104632f2572830bef439d0f778
Mich <paperprimer00@gmail.com>
2024-02-22 13:12:25 -0500

    Setup basic sql queries

---
+++ src/database/DbDef.sql
@@ -0,0 +1,16 @@
+CREATE TYPE Food_Group AS ENUM ('Grain', 'Protein', 'Vegetable', 'Fruit', 'Dairy');
+CREATE TYPE Freshness AS ENUM ('Expired', 'Fresh', 'Near_Expiry');
+
+CREATE TABLE item(
+    name varchar(50) PRIMARY KEY,
+    container varchar(50) REFERENCES container(container_name),
+    quantity INTEGER, 
+    expiry Date,
+    fg Food_Group,
+    fresh Freshness
+);
+
+CREATE TABLE container (
+    container_name varchar(50) PRIMARY KEY
+);
+
