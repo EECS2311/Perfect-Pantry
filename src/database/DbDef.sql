c80ca8dcfeac0356f0b443ea9c9af3129b3586d6
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-02-23 10:10:32 -0500

    Fixed sql queries

--- src/database/DbDef.sql
+++ src/database/DbDef.sql
@@ -1,6 +1,12 @@
 CREATE TYPE Food_Group AS ENUM ('Grain', 'Protein', 'Vegetable', 'Fruit', 'Dairy');
 CREATE TYPE Freshness AS ENUM ('Expired', 'Fresh', 'Near_Expiry');
 
+
+CREATE TABLE container (
+    container_name varchar(50) PRIMARY KEY
+);
+
+
 CREATE TABLE item(
     name varchar(50) PRIMARY KEY,
     container varchar(50) REFERENCES container(container_name),
@@ -8,9 +14,5 @@ CREATE TABLE item(
     expiry Date,
     fg Food_Group,
     fresh Freshness
-);
-
-CREATE TABLE container (
-    container_name varchar(50) PRIMARY KEY
 );
 
--- src/database/init.sql
+++ src/database/init.sql
@@ -6,6 +6,6 @@ VALUES
 
 INSERT INTO item (name, container, quantity, expiry, fg, fresh)
 VALUES
-('GoudaCheese','MainPantry',12, 2024-05-12, 'Dairy', 'Fresh'),
-('FruitLoops', 'MainPantry', 1, 2024-08-1, 'Grain', 'Fresh'),
-('JollyRanchersBox', 'MainPantry', 2, 2024-0-9, 'Fruit', 'Fresh');
+('GoudaCheese','MainPantry',12, '2024-05-12', 'Dairy', 'Fresh'),
+('FruitLoops', 'MainPantry', 1, '2024-08-1', 'Grain', 'Fresh'),
+('JollyRanchersBox', 'MainPantry', 2, '2024-4-14', 'Fruit', 'Fresh');
