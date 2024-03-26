b920066aa5d1d134e63126c5444bbb8430d15535
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-03-26 00:07:08 -0400

    Implement basic graph

--- src/gui/StatsView.java
+++ src/gui/StatsView.java
@@ -19,6 +19,8 @@ public class StatsView extends JPanel {
 	static JFrame frame = new JFrame("Perfect Pantry");
 	static JPanel p = new StatsView();
 	
+	final int BAR_LENGTH = 590;
+	
 
 
 	public static void main(String[] args) {
@@ -43,20 +45,44 @@ public class StatsView extends JPanel {
 	protected void paintComponent(Graphics g) {
 		
 		super.paintComponent(g);
+		
+
+		ArrayList<String>total = HomeView.data.getTotalCount();
+		double size = total.size();
+		double tot = total.size();
+		
+		double protein  = (double)getTotal("Protein", total)/size ;
+		double vegetable  = (double)getTotal("Vegetable", total)/size ;
+		double grain  = (double)getTotal("Grain", total)/size ;
+		double fruit  = (double)getTotal("Fruit", total)/size ;
+		double dairy  = (double)getTotal("Dairy", total)/size ;
+		
 		g.setColor(Color.BLUE);
-		g.fillRect(100, 150, 550,50);
-		//g.drawRect(100, 200, 500, 200);
+		
+		g.fillRect(100, 150, (int)(BAR_LENGTH*protein), 50);
+		
+		g.setColor(Color.RED);
+		g.fillRect(100 + (int)(BAR_LENGTH*protein), 150, (int)(550*vegetable), 50);
+
+		g.setColor(Color.ORANGE);
+		g.fillRect(100 + (int)(BAR_LENGTH*protein) + (int)(BAR_LENGTH*vegetable), 150, (int)(BAR_LENGTH*grain), 50);
+		
+		g.setColor(Color.RED);
+		g.fillRect(100 + (int)(BAR_LENGTH*protein) + (int)(BAR_LENGTH*vegetable) + (int)(BAR_LENGTH*grain), 150, (int)(BAR_LENGTH*fruit), 50);
+		
+		
+		g.setColor(Color.WHITE);
+		g.fillRect(100 + (int)(BAR_LENGTH*protein) + (int)(BAR_LENGTH*vegetable) + (int)(BAR_LENGTH*grain) + (int)(BAR_LENGTH*fruit), 150, (int)(550*dairy), 50);
+		
 		
 	}
 	
 	
 	public StatsView() {
 		
-
 		ArrayList<String>total = HomeView.data.getTotalCount();
+
 	
-		
-		System.out.print(total.size());
 		setBackground(new Color(245, 223, 162));
 		setLayout(null);
 		JComboBox b = new JComboBox();
@@ -94,8 +120,6 @@ public class StatsView extends JPanel {
 	}
 	
 	public int getTotal(String group, ArrayList<String> s) {
-		
-			
 		
 		return s.stream().filter(x -> x!= null && x.equals(group)).toList().size();
 		
