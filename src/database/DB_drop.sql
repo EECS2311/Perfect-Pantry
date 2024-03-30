a25efb1192d2586c160887ef836e7b4d3b9bfe02
nina2dev <nina2dev@gmail.com>
2024-03-29 20:01:36 -0400

    Make cascade consistent

--- src/database/DB_drop.sql
+++ src/database/DB_drop.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS "container" cascade;
+DROP TABLE IF EXISTS "container" CASCADE;
 DROP TABLE IF EXISTS "item";
 DROP TABLE IF EXISTS "storage_tips";
 DROP TABLE IF EXISTS "grocery";
