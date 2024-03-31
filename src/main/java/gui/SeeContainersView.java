b1f9d4b8f535b4aa47c163960ab2950d325375c4
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-03-31 14:33:50 -0400

    Complete merge, refactor imports

--- src/main/java/gui/SeeContainersView.java
+++ src/main/java/gui/SeeContainersView.java
@@ -1,89 +1,267 @@
-package main.java.domain.logic.recipe;
+package main.java.gui;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.plaf.FontUIResource;
+
+import main.java.domain.logic.container.Container;
+import main.java.domain.logic.container.ContainerUtility;
 import main.java.gui.home.HomeView;
 
 /**
- * Utility class for managing recipes, including checking for ingredient updates and finding recipes lazily.
+ * The GUI which shows the list of Containers the user made,
+ *  and is able to go to deleteContainerView, editContainerView, HomeView and ItemsView
  */
-public class RecipeUtility {
+public class SeeContainersView implements ActionListener{
 
-    /**
-     * Checks if the set of near expiry ingredients has changed compared to the current ingredients
-     * and updates the ingredient set if there are changes.
-     *
-     * @param ingredients The set of ingredients to check and update.
-     * @return true if the ingredients set was updated with new items, false if no changes were made.
-     */
-    public static boolean isNearExpiryItemsChanged(Set<String> ingredients) {
-        Set<String> currentIngredients = HomeView.data.getNearExpiryOrFreshItemNames();
-        if (!currentIngredients.equals(ingredients)) {
-            ingredients.clear();
-            ingredients.addAll(currentIngredients);
-            return true;
-        }
-        return false;
-    }
+	
+	/**
+	 * Hold buttons pertaining to its containers
+	 */
+	private JPanel containerButtonsPanel = new JPanel();
 
-    /**
-     * Finds recipes lazily based on a set of ingredients. If the non-expired items have changed,
-     * it queries the external Recipe API for new recipes based on these ingredients and updates
-     * the provided `recipes` list; otherwise, it retains the cached recipes in the `recipes` list.
-     *
-     * @param ingredients A set of ingredient names (non-null) to be used for finding recipes.
-     * @param recipes The current list of recipes (cached). This list will be cleared and updated
-     *                with new recipes if the non-expired items have changed since the last query.
-     *                The list should not be null, but it can be empty.
-     * @throws RateLimitPerMinuteExceededException If the rate limit for querying the Recipe API
-     *                                             per minute is exceeded.
-     * @throws DailyLimitExceededException If the daily limit for querying the Recipe API is exceeded.
-     * @throws IOException If an I/O error occurs while communicating with the Recipe API.
-     */
-    public static void findRecipesLazyLoad(Set<String> ingredients, List<Recipe> recipes) throws RateLimitPerMinuteExceededException, IOException, DailyLimitExceededException {
-        if(isNearExpiryItemsChanged(ingredients)){
-            String ingredientsString = String.join(", ", ingredients);
-            List<Recipe> newRecipes = RecipeApiClient.findRecipesByIngredients(ingredientsString, 5);
+	/**
+	 * Allows containerButtonPanel to be Scrollable
+	 */
+	JScrollPane pane = new JScrollPane(containerButtonsPanel);
 
-            recipes.clear();
-            recipes.addAll(newRecipes);
-        }
-    }
+	/**
+	 * Holds components for the container list view
+	 */
+	private JPanel viewOfContainerPanel = new JPanel();
 
-    /**
-     * Attempts to save a recipe to the database if it does not already exist.
-     *
-     * This method checks whether a recipe with the same ID already exists in the database.
-     * If the recipe is new, it saves the recipe to the database and returns true.
-     * If the recipe already exists, it does not perform any operation and returns false.
-     *
-     * @param recipe The Recipe object to save to the database.
-     * @return true if the recipe was successfully saved (i.e., it did not exist in the database),
-     *         false if the recipe already exists in the database and was not saved.
-     */
-    public static boolean verifySaveRecipeToDatabase(Recipe recipe){
-        boolean isRecipeExists = RecipeUtility.isRecipeInDatabase(recipe.getId());
-        if (!isRecipeExists) {
-            HomeView.data.saveRecipeToDatabase(recipe);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * Holds button on container list view
+	 */
+	private JPanel backPanel = new JPanel();
 
-    /**
-     * Checks if a recipe with a specific ID exists in the database.
-     *
-     * This method queries the database to determine whether a recipe with the given ID
-     * is already stored. It is used internally to avoid duplicating recipes in the database.
-     *
-     * @param recipeId The unique identifier of the recipe to check in the database.
-     * @return true if the recipe exists in the database, false otherwise.
-     */
-    private static boolean isRecipeInDatabase(int recipeId) {
-        return HomeView.data.isRecipeInDatabase(recipeId);
-    }
+	/**
+	 * Button to go from container view to home
+	 */
+	private JButton viewOfContainer2HomeButton = new JButton("Back");
+
+	/**
+	 * Popup for edit or deleting container button
+	 */
+	private JPopupMenu popup;
+	
+	/**
+	 * Option for popup to rename container button
+	 */
+	private JMenuItem renameContainerBtn = new JMenuItem("Rename Container");
+	
+	/**
+	 * Option for container to remove container button
+	 */
+	private JMenuItem removeContainerBtn = new JMenuItem("Delete Container");
+	
+	/**
+	 * Holds button which has been pressed
+	 */
+	private JButton buttonPressed;
+	
+	/**
+	 * Holds this instance of SeeContainersView
+	 */
+	private static SeeContainersView containersView;
+	
+	/**
+	 * Font for components
+	 */
+	private Font font;
+	
+	/**
+	 * Scrollable for backPanel
+	 */
+	JScrollPane backScrollPanel = new JScrollPane(backPanel);
+
+
+
+	/**
+	 * Launches the application and initializes the main GUI components.
+	 */
+	public SeeContainersView() {
+		containersView = this;
+	}
+
+
+	/**
+	 * Sets the visibility of the SeeContainersView GUI depending on the boolean passed
+	 * @param b the value of whether the visibility is true or not
+	 */
+
+	public void setSeeContainersViewVisibility(boolean b) {
+		if (b == true) { 
+			font = new FontUIResource("Dialog", Font.PLAIN, HomeView.getSettings().getFontSize());
+
+			HomeView.getHomeView().setHomeViewVisibility(false);
+			viewOfContainer2HomeButton.addActionListener(this);
+			viewOfContainer2HomeButton.setFont(font);
+			
+			HomeView.getFrame().add(viewOfContainerPanel);
+			viewOfContainerPanel.setLayout(null);
+			viewOfContainerPanel.setBackground(new Color(253, 241, 203));
+
+			backPanel.setBackground(new Color(253, 241, 203));
+			backPanel.setLayout(new FlowLayout());
+			backPanel.add(viewOfContainer2HomeButton);
+			
+			backScrollPanel.setBounds(0, 0, 800, 50);
+			viewOfContainerPanel.add(backScrollPanel);
+
+			containerButtonsPanel.setBounds(0, 50, 800, 500);
+			containerButtonsPanel.setBackground(new Color(253, 241, 200));
+			containerButtonsPanel.setLayout(new GridLayout((HomeView.getContainerMap().size()/3 +1), 3));
+
+
+			containerButtonsPanel.removeAll();
+
+			popup = new JPopupMenu();
+			removeContainerBtn = new JMenuItem("Delete Container");
+			removeContainerBtn.addActionListener(this);
+
+			renameContainerBtn = new JMenuItem("Rename Container");
+			renameContainerBtn.addActionListener(this);
+
+
+			popup.add(renameContainerBtn);
+			popup.add(removeContainerBtn);
+
+			HomeView.getContainerMap().forEach((button, container) -> {
+				containerButtonsPanel.add(button);
+				button.addActionListener(this);
+				button.setComponentPopupMenu(popup);
+				button.setFont(font);
+				button.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent p) {
+						Boolean bool = SwingUtilities.isRightMouseButton(p);
+						if (bool) {	
+							buttonPressed = button;
+						}
+					}
+
+				});
+			});
+
+			viewOfContainerPanel.add(pane);
+			pane.setBounds(0, 50, 800, 500);
+
+			viewOfContainerPanel.setVisible(true);
+			
+
+		}
+		if (b == false) {
+			//Remove Action listeners
+			HomeView.getContainerMap().forEach((button, container) -> {
+				button.removeActionListener(this);
+
+			});
+			renameContainerBtn.removeActionListener(this);
+			removeContainerBtn.removeActionListener(this);
+			viewOfContainer2HomeButton.removeActionListener(this);
+			viewOfContainerPanel.setVisible(false);
+
+		}
+	}
+
+	/**
+	 * Initiates the container renaming process for a given container button.
+	 * 
+	 * @param b The button corresponding to the container to be renamed.
+	 */
+	public void renameContainer(JButton b) {
+		Container c = HomeView.getContainerMap().get(b);
+		if(c != null) {
+			String nameOfContainer = JOptionPane.showInputDialog(HomeView.getFrame(),
+					"What would you like to rename container " + c.getName() + " to?");
+
+			ContainerUtility.verifyEditContainer(c.getName(), nameOfContainer, HomeView.data, b, HomeView.getContainerMap(),
+					(errorMsg) -> JOptionPane.showMessageDialog(HomeView.getFrame(), errorMsg, "Input Error", JOptionPane.ERROR_MESSAGE),
+					() -> {
+						c.setName(nameOfContainer);
+						// Update the button text directly instead of replacing the button in the map
+						b.setText(c.getName());
+						viewOfContainerPanel.revalidate();
+//						HomeView.getHomeView().setHomeViewVisibility(true);
+					});
+		}
+	}
+
+	/**
+	 * Deletes a container from the data and its map
+	 * 
+	 * @param b the button corresponding to the container to be deleted
+	 */
+	public void removeContainer(JButton b) {
+		Container c = HomeView.getContainerMap().get(b);
+		if(c!= null) {
+			int opt = JOptionPane.showConfirmDialog(HomeView.getFrame(), "Delete Container \"" + c.getName() + "\"?");
+			if (opt == JOptionPane.YES_OPTION) { // if not cancelled
+				ContainerUtility.verifyDeleteContainer(c.getName(), HomeView.data, b, HomeView.getContainerMap(), (errorMsg) -> JOptionPane
+						.showMessageDialog(HomeView.getFrame(), errorMsg, "Input Error", JOptionPane.ERROR_MESSAGE), () -> {
+//							HomeView.getHomeView().setHomeViewVisibility(true);
+							viewOfContainerPanel.revalidate();
+						});
+
+				c = null;
+
+			}
+		}
+	}
+	/**
+	 * Handles action events triggered by various GUI components.
+	 * This method is the central hub for processing user interactions within the home view.
+	 *
+	 * @param e The ActionEvent object containing details about the event.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+
+		if (source == viewOfContainer2HomeButton) {
+			HomeView.getHomeView().setHomeViewVisibility(true);
+		}
+
+		else if(source == renameContainerBtn) {
+			renameContainer(buttonPressed);
+		}
+
+		else if(source == removeContainerBtn) {
+			removeContainer(buttonPressed);
+		}
+		else {
+			Container c = HomeView.getContainerMap().get(source); // This will return null if the button is not found
+			if (c != null) {
+				c.getGUI();
+			}
+
+		}
+
+	}
+
+
+	/**
+	 * Provide Accces to this instance of SeeContainersView
+	 * 
+	 * @return the current instance of SeeContainersView
+	 */
+	public static SeeContainersView getContainersView() {
+		return containersView;
+	}
+	
 
 }
